1f6502af5d981cf55dd02693caa726186ea7136f
IdCragEon <neil851129@gmail.com>
2019-12-19 21:28:07 +0800

    Update

---
+++ Train.py
@@ -0,0 +1,239 @@
+import os
+import sys
+import itertools
+import math
+import logging
+import json
+import re
+import random
+from collections import OrderedDict
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.lines as lines
+from matplotlib.patches import Polygon
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("./Mask_RCNN/")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+from mrcnn.model import log
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+import glob
+
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+class SupercategoryConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "supercategory"
+    
+    BACKBONE = "resnet50"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 4
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 20  # background + 20
+
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 64
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+    
+config = SupercategoryConfig()
+config.display()
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+TRAIN_DATA_DIR = "train_images/"
+coco = COCO("pascal_train.json") 
+class SupercategoryDataset(utils.Dataset):
+    """Generates the shapes synthetic dataset. The dataset consists of simple
+    shapes (triangles, squares, circles) placed randomly on a blank surface.
+    The images are generated on the fly. No file access required.
+    """
+
+    def load_supercategory(self, return_=False):
+        
+        global coco 
+        
+        class_ids = sorted(coco.getCatIds())
+        image_ids = list(coco.imgs.keys())
+        
+        # Add classes
+        for i in class_ids:
+            self.add_class("supercategory", i, coco.loadCats(i)[0]["name"])
+
+        # Add images
+        for i in image_ids:
+            self.add_image(
+                "supercategory", image_id=i,
+                path=os.path.join(TRAIN_DATA_DIR, coco.imgs[i]['file_name']),
+                width=coco.imgs[i]["width"],
+                height=coco.imgs[i]["height"],
+                annotations=coco.loadAnns(coco.getAnnIds(
+                    imgIds=[i], catIds=class_ids, iscrowd=None)))
+        if return_:
+            return coco
+
+
+    def load_mask(self, image_id):
+        """Generate instance masks for shapes of the given image ID.
+        """
+        global coco 
+        image_info = self.image_info[image_id]
+        
+        instance_masks = []
+        class_ids = []
+        annotations = self.image_info[image_id]["annotations"]
+        
+        for annotation in annotations:
+            class_id = self.map_source_class_id(
+                "supercategory.{}".format(annotation['category_id']))
+            if class_id:
+                m = coco.annToMask(annotation)
+                # Some objects are so small that they're less than 1 pixel area
+                # and end up rounded out. Skip those objects.
+                if m.max() < 1:
+                    continue
+                # Is it a crowd? If so, use a negative class ID.
+                if annotation['iscrowd']:
+                    # Use negative class ID for crowds
+                    class_id *= -1
+                    # For crowd masks, annToMask() sometimes returns a mask
+                    # smaller than the given dimensions. If so, resize it.
+                    if m.shape[0] != image_info["height"] or m.shape[1] != image_info["width"]:
+                        m = np.ones([image_info["height"], image_info["width"]], dtype=bool)
+                instance_masks.append(m)
+                class_ids.append(class_id)
+        mask = np.stack(instance_masks, axis=2).astype(np.bool)
+        class_ids = np.array(class_ids, dtype=np.int32)
+        return mask, class_ids
+
+# Training dataset
+dataset_train = SupercategoryDataset()
+dataset_train.load_supercategory()
+dataset_train.prepare()
+
+
+print("Image Count: {}".format(len(dataset_train.image_ids)))
+print("Class Count: {}".format(dataset_train.num_classes))
+for i, info in enumerate(dataset_train.class_info):
+    print("{:3}. {:50}".format(i, info['name']))
+
+
+# Load and display random samples
+image_ids = np.random.choice(dataset_train.image_ids, 4)
+for image_id in image_ids:
+    image = dataset_train.load_image(image_id)
+    mask, class_ids = dataset_train.load_mask(image_id)
+    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+
+
+# Load random image and mask.
+image_id = random.choice(dataset_train.image_ids)
+image = dataset_train.load_image(image_id)
+mask, class_ids = dataset_train.load_mask(image_id)
+# Compute Bounding box
+bbox = utils.extract_bboxes(mask)
+
+# Display image and additional stats
+print("image_id ", image_id, dataset_train.image_reference(image_id))
+log("image", image)
+log("mask", mask)
+log("class_ids", class_ids)
+log("bbox", bbox)
+# Display image and instances
+visualize.display_instances(image, bbox, mask, class_ids, dataset_train.class_names)
+
+# Load random image and mask.
+image_id = np.random.choice(dataset_train.image_ids, 1)[0]
+image = dataset_train.load_image(image_id)
+mask, class_ids = dataset_train.load_mask(image_id)
+original_shape = image.shape
+# Resize
+image, window, scale, padding, _ = utils.resize_image(
+    image, 
+    min_dim=config.IMAGE_MIN_DIM, 
+    max_dim=config.IMAGE_MAX_DIM,
+    mode=config.IMAGE_RESIZE_MODE)
+mask = utils.resize_mask(mask, scale, padding)
+# Compute Bounding box
+bbox = utils.extract_bboxes(mask)
+
+# Display image and additional stats
+print("image_id: ", image_id, dataset_train.image_reference(image_id))
+print("Original shape: ", original_shape)
+log("image", image)
+log("mask", mask)
+log("class_ids", class_ids)
+log("bbox", bbox)
+# Display image and instances
+visualize.display_instances(image, bbox, mask, class_ids, dataset_train.class_names)
+
+
+
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+
+
+# Which weights to start with?
+init_with = "last"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last(), by_name=True)
+
+model.train(dataset_train,dataset_train,
+            learning_rate=config.LEARNING_RATE, 
+            epochs=40, 
+            layers='heads')
+
+
+
